9b3d49fd526a676f658c4da52671311ec6a77d0e
Andrew Berry <deviantintegral@gmail.com>
2017-10-18 11:35:31 -0400

    Fix exit status causing false fail

--- test.sh
+++ test.sh
@@ -4,21 +4,27 @@
 git init test_this_branch
 cd test_this_branch
 ../setup.sh $1
+set +e
 egrep -r '(my_module|MyModule)' * .circleci .gitignore
 CHECK=$?
-if [ $? ]
+if [ ! $? ]
 then
   exit 1
 fi
+set -e
 cd ..
 
 # Now test using this code to fetch the last tag
 git init test_last_tag
 cd test_last_tag
 ../setup.sh
+set +e
 egrep -r '(my_module|MyModule)' * .circleci .gitignore
 CHECK=$?
-if [ $? ]
+if [ ! $? ]
 then
   exit 1
 fi
+set -e
+
+echo 'All tests have passed.'

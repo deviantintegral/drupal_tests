582e8dd0fb25db42d52735010ec4d0cbe4ea26b6
Andrew Berry <deviantintegral@gmail.com>
2017-10-18 11:20:10 -0400

    Use this repo for test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # Test first manually specifying a branch
 git init test_this_branch
 cd test_this_branch
-curl -L https://github.com/deviantintegral/drupal_tests/raw/$1/setup.sh test_this_branch | bash
+../setup.sh | bash
 if [ $(egrep -r (my_module|MyModule) * .circleci .gitignore) ]
 then
   exit 1
@@ -13,7 +13,7 @@ cd ..
 # Now test using this code to fetch the last tag
 git init test_last_tag
 cd test_last_tag
-curl -L https://github.com/deviantintegral/drupal_tests/raw/$1/setup.sh | bash
+../setup.sh | bash
 if [ $(egrep -r (my_module|MyModule) * .circleci .gitignore) ]
 then
   exit 1

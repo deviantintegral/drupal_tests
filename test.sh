3b787374081de94b51582ae77d29840f2fa332a4
Andrew Berry <deviantintegral@gmail.com>
2017-10-18 11:21:18 -0400

    Fix wrong pipe

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # Test first manually specifying a branch
 git init test_this_branch
 cd test_this_branch
-../setup.sh | bash
+../setup.sh
 if [ $(egrep -r (my_module|MyModule) * .circleci .gitignore) ]
 then
   exit 1
@@ -13,7 +13,7 @@ cd ..
 # Now test using this code to fetch the last tag
 git init test_last_tag
 cd test_last_tag
-../setup.sh | bash
+../setup.sh
 if [ $(egrep -r (my_module|MyModule) * .circleci .gitignore) ]
 then
   exit 1

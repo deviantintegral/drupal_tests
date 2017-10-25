b3e44964a3f2b2d2492d7faea930beff0c5faf68
Andrew Berry <deviantintegral@gmail.com>
2017-10-25 13:08:33 -0400

    Use report-junit to prevent redirecting errors to the output file

--- hooks/code-sniffer.sh
+++ hooks/code-sniffer.sh
@@ -16,4 +16,4 @@ vendor/bin/phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer
 vendor/bin/phpmd modules/$1/src html cleancode,codesize,design,unusedcode --ignore-violations-on-exit --reportfile artifacts/phpmd/index.html
 vendor/bin/phpmetrics --extensions=php,inc,module --report-html=artifacts/phpmetrics --git modules/$1
 # Check coding standards
-vendor/bin/phpcs --standard=Drupal --report=junit modules/$1 > artifacts/phpcs/phpcs.xml
+vendor/bin/phpcs --standard=Drupal --report=junit --report-junit=artifacts/phpcs/phpcs.xml modules/$1

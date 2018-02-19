21ba783df0e58fc663725e1ce28db0608d9222f9
Andrew Berry <deviantintegral@gmail.com>
2018-02-19 16:26:10 -0500

    Do all the tests

--- test.sh
+++ test.sh
@@ -15,20 +15,15 @@ fi
 
 sudo php composer-setup.php --quiet --install-dir=/usr/local/bin --filename=composer
 
+sudo curl -o /usr/local/bin/circleci.sh https://circle-downloads.s3.amazonaws.com/releases/build_agent_wrapper/circleci && sudo chmod +x /usr/local/bin/circleci.sh
+
 git clone git@github.com:nodespark/elasticsearch_connector.git
 cd elasticsearch_connector
 git checkout 22ac399
-../setup.sh $1
-
-sudo curl -o /usr/local/bin/circleci.sh https://circle-downloads.s3.amazonaws.com/releases/build_agent_wrapper/circleci && sudo chmod +x /usr/local/bin/circleci.sh
-
-circleci.sh -e CIRCLE_PROJECT_REPONAME=elasticsearch_connector build --job run-behat-tests
 
 # Test first manually specifying a branch
-git init test_this_branch
-cd test_this_branch
-composer init -n --name=example/test
 ../setup.sh $1
+
 circleci config validate
 set +e
 egrep -r '(my_module|MyModule)' * .circleci .gitignore
@@ -38,13 +33,17 @@ then
   exit 1
 fi
 set -e
-cd ..
+
+circleci.sh -e CIRCLE_PROJECT_REPONAME=elasticsearch_connector build --job run-code-sniffer
+circleci.sh -e CIRCLE_PROJECT_REPONAME=elasticsearch_connector build --job run-unit-kernel-test
+BRANCH=$(circleci.sh -e CIRCLE_PROJECT_REPONAME=elasticsearch_connector build --job run-behat-tests)
+echo $BRANCH | grep "1 scenario (1 passed)"
 
 # Now test using this code to fetch the last tag
-git init test_last_tag
-cd test_last_tag
-composer init -n --name=example/test
+git reset --hard HEAD
+git clean -dxf .
 ../setup.sh
+
 circleci config validate
 set +e
 egrep -r '(my_module|MyModule)' * .circleci .gitignore
@@ -54,5 +53,10 @@ then
   exit 1
 fi
 set -e
+
+circleci.sh -e CIRCLE_PROJECT_REPONAME=elasticsearch_connector build --job run-code-sniffer
+circleci.sh -e CIRCLE_PROJECT_REPONAME=elasticsearch_connector build --job run-unit-kernel-test
+BRANCH=$(circleci.sh -e CIRCLE_PROJECT_REPONAME=elasticsearch_connector build --job run-behat-tests)
+echo $BRANCH | grep "1 scenario (1 passed)"
 
 echo 'All tests have passed.'

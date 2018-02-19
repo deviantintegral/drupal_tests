939bc457239fc4359f9fd470fb19ed9c0674e712
Andrew Berry <deviantintegral@gmail.com>
2018-02-19 16:06:21 -0500

    Add PHP5 and composer

--- test.sh
+++ test.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -ex
 
+sudo apt-get update -y
+sudo apt-get install php5-cli -y
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+mv composer.phar /usr/local/bin
+
 git clone git@github.com:nodespark/elasticsearch_connector.git
 cd elasticsearch_connector
 git checkout 22ac399

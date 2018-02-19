c90d1f47e54b142af999e40bbe67d82e24744fb2
Andrew Berry <deviantintegral@gmail.com>
2018-02-19 10:27:30 -0500

    Fix underscore

--- hooks/update-dependencies.sh
+++ hooks/update-dependencies.sh
@@ -10,4 +10,4 @@ robo update:dependencies
 # Touch a flag so we know dependencies have been set. Otherwise, there is no
 # easy way to know this step needs to be done when running circleci locally since
 # it does not support workflows.
-touch dependencies-updated
+touch dependencies_updated

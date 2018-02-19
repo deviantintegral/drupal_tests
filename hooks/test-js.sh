18292291610231dfafc5d4f2aa089676703b12a8
Andrew Berry <deviantintegral@gmail.com>
2018-02-19 11:39:06 -0500

    Give mariadb time to boot up

--- hooks/test-js.sh
+++ hooks/test-js.sh
@@ -12,6 +12,9 @@ fi
 # This is the command used by the base image to serve Drupal.
 apache2-foreground&
 
+# Wait for the mariadb container to come up.
+while ! mysqladmin ping -h127.0.0.1; do sleep 1; done
+
 robo setup:drupal || true
 
 chown -R www-data:www-data /var/www/html/sites/default/files
